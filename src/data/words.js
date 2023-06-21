ec41b359e9e2cac0a80fea4e6be5db31a95beb8f
Lucas Campos <lucash_c@yahoo.com.br>
2023-06-21 17:07:41 -0300

    ajustes

--- src/data/words.js
+++ src/data/words.js
@@ -1,8 +1,8 @@
 export const wordsList = {
-  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Roda"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Limão"],
   corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
-  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
-  programação: ["Linguagem", "Framework", "JavaScript", "React"],
+  computador: ["Mouse", "Bios", "Monitor", "Memoria"],
+  programação: ["Java", "VueJs", "NodeJs", "React"],
   alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
 };
